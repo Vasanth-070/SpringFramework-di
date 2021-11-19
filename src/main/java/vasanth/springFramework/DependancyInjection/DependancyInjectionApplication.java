017b2f49eacd550923c99f4a5573bd49532e1919
Vasanth-070 <vasanthkzoomin@gmail.com>
2021-11-19 12:15:25 +0530

    maven setup

---
+++ src/main/java/vasanth/springFramework/DependancyInjection/DependancyInjectionApplication.java
@@ -0,0 +1,13 @@
+package vasanth.springFramework.DependancyInjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependancyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependancyInjectionApplication.class, args);
+	}
+
+}
